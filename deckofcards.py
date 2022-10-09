68cd53bbb18b51a8b318b6b0164715df9d380d44
devin803 <Dinocencio@student.bridgew.edu>
2022-10-09 01:47:12 -0400

    Created methods in the Card class to show to value and suit

--- deckofcards.py
+++ deckofcards.py
@@ -1,8 +1,11 @@
 class Card:
 
-    def __init__(self):
-        pass
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
 
+    def __str__(self):
+        return '{} of {}'.format(self.value, self.suit)
 
 class Deck:
 
@@ -12,5 +15,8 @@ class Deck:
 
 class Player:
 
-    def __init__(self):
+    def __init__(self,name):
         pass
+
+card = Card("Hearts", 6)
+print(card)
